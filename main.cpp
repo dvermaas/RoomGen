d7d2a4b3fb3afe8803ac5cd289e7fa9061eb6cb9
Dvermaas <31657287+dvermaas@users.noreply.github.com>
2024-11-08 23:24:00 +0100

    less lines

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include <iostream>
 #include <random>
 #include <algorithm>
-#include <unordered_set>
 
 using namespace std;
 
@@ -20,21 +19,16 @@ private:
     vector<Room *> grid;
     vector<int> queue;
     array<int, 4> offset;
-    random_device rd;
-    mt19937 rng;
 
 public:
-    explicit Level(const int size) : size(size), grid(size * size, nullptr), offset{-size, 1, size, -1}, rng(rd()) {
+    explicit Level(const int size) : size(size), grid(size * size, nullptr), offset{-size, 1, size, -1} {
         const int middle = (size / 2) * size + (size / 2);
         constexpr array<bool, 4> doors = {true, true, true, true};
         grid[middle] = new Room(doors);
 
         for (int i = 0; i < 4; ++i) {
-            if (doors[i]) {
-                queue.push_back(middle + offset[i]);
-            }
+            if (doors[i]) queue.push_back(middle + offset[i]);
         }
-
 
         while (!queue.empty()) {
             generate();
@@ -56,16 +50,11 @@ public:
     void addRoom(const int o) {
         cout << "Adding room :" << o << endl;
         // stop when illegal position
-        if (grid[o] != nullptr) {
-            raise(*"Incorrect room added");
-            return;
-        }
+        if (grid[o] != nullptr) raise(*"Incorrect room added");
 
         // randomize doors
         array<bool, 4> doors{};
-        for (int i = 0; i < 4; ++i) {
-            doors[i] = rand() % 2;
-        }
+        for (int i = 0; i < 4; ++i) doors[i] = rand() % 2;
 
         for (int i = 0; i < 4; ++i) {
             const int neighbor_index = o + offset[i];
@@ -77,27 +66,14 @@ public:
 
             const Room *neighbour = grid[neighbor_index];
             // create door if neighbour has door
-            if (neighbour != nullptr && neighbour->doors[(i + 2) % 4]) {
-                doors[i] = true;
-            }
+            if (neighbour != nullptr && neighbour->doors[(i + 2) % 4]) doors[i] = true;
             // remove illegal doors
-            if (i == 0 and i / size == 0) {
-                doors[i] = false;
-            };
-            if (i == 1 and i % size == size - 1) {
-                doors[i] = false;
-            };
-            if (i == 2 and i / size == size - 1) {
-                doors[i] = false;
-            };
-            if (i == 3 and i % size == 0) {
-                doors[i] = false;
-            };
+            if (i == 0 and i / size == 0) doors[i] = false;
+            if (i == 1 and i % size == size - 1) doors[i] = false;
+            if (i == 2 and i / size == size - 1) doors[i] = false;
+            if (i == 3 and i % size == 0) doors[i] = false;
             // doors to the void should be put in the queue
-            if (neighbour == nullptr and doors[i]) {
-                cout << "adding to queue: " << neighbor_index << endl;
-                queue.push_back(neighbor_index);
-            }
+            if (neighbour == nullptr and doors[i]) queue.push_back(neighbor_index);
         };
 
         grid[o] = new Room(doors);
@@ -107,7 +83,7 @@ public:
 
     void printQueue() const {
         std::cout << "Queue:";
-        for (int value: queue) {
+        for (const int value: queue) {
             std::cout << value << " ";
         }
         std::cout << std::endl;
@@ -118,8 +94,8 @@ public:
 
         for (int i = 0; i < grid.size(); ++i) {
             if (const Room *room = grid[i]; room != nullptr) {
-                int x = (i % size) * 2;
-                int y = (i / size) * 2;
+                const int x = (i % size) * 2;
+                const int y = (i / size) * 2;
                 display_grid[y][x] = 'X';
                 if (room->doors[0]) display_grid[y - 1][x] = '|';
                 if (room->doors[1]) display_grid[y][x + 1] = '-';
